711cefbba1000c46f702f814f029bda7f94356e3
donovanmuller <donovan.muller@gmail.com>
2014-04-30 18:08:30 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+play-helloworld-dynamic-title
+=============================
+
+See https://groups.google.com/d/msg/play-framework/nH_ro2vghKA/ew4BWfGqkakJ
+
+Play! sample [Hello world](https://github.com/playframework/playframework/tree/master/samples/scala/helloworld) app with page title using form value.
+
+Added new form field for page title:
+
+![New Form Property](http://i.imgur.com/N0nWZbE.png)
+
+See title and <header> using the value typed in:
+
+![See title](http://i.imgur.com/D8D95Co.png)
---
+++ app/controllers/Application.scala
@@ -0,0 +1,43 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+
+import views._
+
+object Application extends Controller {
+
+  /**
+   * Describes the hello form.
+   */
+  val helloForm = Form(
+    tuple(
+      "pageTitle" -> nonEmptyText,
+      "name" -> nonEmptyText,
+      "repeat" -> number(min = 1, max = 100),
+      "color" -> optional(text)
+    )
+  )
+
+  // -- Actions
+
+  /**
+   * Home page
+   */
+  def index = Action {
+    Ok(html.index(helloForm))
+  }
+
+  /**
+   * Handles the form submission.
+   */
+  def sayHello = Action { implicit request =>
+    helloForm.bindFromRequest.fold(
+      formWithErrors => BadRequest(html.index(formWithErrors)),
+      {case (pageTitle, name, repeat, color) => Ok(html.hello(pageTitle, name, repeat.toInt, color))}
+    )
+  }
+
+}
